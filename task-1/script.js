4fe50d1f22d1b20c0b3bcb8ba28f963a74fcbe89
Azim Islam <65490972+Azim-Islam@users.noreply.github.com>
2024-11-11 14:25:21 +0600

    Update script.js

--- task-1/script.js
+++ task-1/script.js
@@ -5,24 +5,46 @@ const actionBtn = document.getElementById('actionBtn');
 const resetBtn = document.getElementById('resetBtn');
 const text = document.getElementById('text');
 
-const animationTime = 5000;
+const animationTime = 3000;
 
-function showAnimation(animationTime){
-    loaderBar.style.animationDuration = `${animationTime/1000}s`;
+function showAnimation(animationTime) {
+    loaderBar.style.animationDuration = `${animationTime / 1000}s`;
     console.log(loaderBar);
     loaderContainer.classList.replace('d-none', 'd-block');
 }
 
-function hideAnimation(){
+function hideAnimation() {
     loaderContainer.classList.replace('d-block', 'd-none');
     text.style.background = 'black';
     text.style.color = 'white';
 }
 
 
-actionBtn.addEventListener('click', () => {
+// actionBtn.addEventListener('click', () => {
+//     showAnimation(animationTime);
+//     setTimeout(hideAnimation, animationTime);
+//     // await waitFuynction(2000);
+//     // hideAnimation();
+// });
+
+function genSleep(animationTime) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(() => { resolve('Sleep Time Ended') }, animationTime);
+    });
+}
+
+async function asyncStart(animationTime) {
     showAnimation(animationTime);
-    setTimeout(hideAnimation, animationTime);
+    await genSleep(animationTime); // animationTime is the amount pause the exection
+    hideAnimation();
+    console.log("Await Ended");
+}
+
+actionBtn.addEventListener('click', () => {
+    console.log("Before Await");
+    asyncStart(animationTime);
+    console.log("After Await");
+
 });
 
 resetBtn.addEventListener('click', () => {
